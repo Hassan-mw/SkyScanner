e525d578481286a15093f6a8b3d4d78a5dea2161
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-06-17 23:39:47 +0500

    Updating Frontend Data

--- app/flights/FlightComponents/PlaneFlights.tsx
+++ app/flights/FlightComponents/PlaneFlights.tsx
@@ -54,13 +54,13 @@ const filterArray=flightsArray
   
   }
     return (
-        <div className="w-full flex  min-h-full lg:flex  lg:space-x-10 bg-[#eff3f8] ">
+        <div className="w-full flex  min-h-full lg:flex items-center justify-center lg:items-start lg:justify-start  lg:space-x-3 bg-[#eff3f8] ">
        
         {/* Show sidebar at alarge */}
-        <div className="hidden lg:block min-w-[300px] "><SideBarFilter range={range} setRange={setRange}  setCheckStops={setCheckStops} /> </div>  
+        <div className="hidden lg:block min-w-[230px] b "><SideBarFilter range={range} setRange={setRange}  setCheckStops={setCheckStops} /> </div>  
         
         {/* //!  Main_body */}
-        <div className="w-full h-full  flex flex-col items-center justify-center  max-w-screen-lg ">
+        <div className="w-full h-full  lg:max-w-xl  flex flex-col items-center justify-center  max-w-[900px] bg-blue-300 ">
           
           
         {/* Top filter show at large */}
@@ -73,7 +73,7 @@ const filterArray=flightsArray
 
 
         {/*//!  Main_Body  */}
-        <div className="w-full h-full grid grid-cols-1  gap-3 p-3 ">
+        <div className="w-full h-full grid grid-cols-1 gap-3 p-3 ">
         {filterArray.map((data,index)=>
 
         <div key={index} className=" w-full  p-2  bg-white  shadow-lg rounded-2xl flex items-center justify-center space-x-4 border ">
@@ -81,10 +81,10 @@ const filterArray=flightsArray
         <div className="w-full grid grid-cols-1 sm:grid-cols-3   items-center justify-center space-y-1  ">
        
         {/* Plane_Deport_Return */}
-        <div className="w-full flex flex-col  sm:col-span-2 items-center justify-center space-y-1 md:space-y-0 md:space-x-2 sm:pr-2 ">
+        <div className="w-full pl-2 max-w-[80%]  flex flex-col  sm:col-span-2 items-center justify-center space-y-1 md:space-y-0 md:space-x-2  ">
           
           {/* 1 */}
-          <div className="w-full  flex items-center justify-between   gap-x-12  px-3  ">
+          <div className="w-full  flex items-center justify-between   gap-x-12    ">
               <div><Image height={90} width={90}  className=" " src="/pia.png" alt="pia-logo"/></div>
               <div className="w-full flex  items-center justify-center space-x-3">
                 
@@ -117,7 +117,7 @@ const filterArray=flightsArray
           </div>
           
           {/* 2 */}
-          <div className="w-full flex  items-center justify-between border-b pb-2 border-slate-300 sm:border-none gap-x-12  px-3  ">
+          <div className="w-full flex  items-center justify-between border-b pb-2 border-slate-300 sm:border-none gap-x-12   ">
               <div><Image height={90} width={90}  className=" " src="/pia.png" alt="pia-logo"/></div>
               <div className="w-full flex  items-center justify-center space-x-3">
                 
@@ -151,20 +151,23 @@ const filterArray=flightsArray
         </div>
           
         {/* Plane_Price_Data */}
-          <div className="w-full  sm:col-span-1 bg-red-400   flex items-end justify-end sm:items-center sm:justify-center  sm:border-l border-slate-900  py-2 ">
+          <div className="w-full  sm:col-span-1    flex items-end justify-end sm:items-center sm:justify-center  sm:border-l border-[#e5e5e5]  py-2 ">
           {/*Pricing_btton  */}
-          <div className="flex sm:flex-col items-center justify-center space-x-2">
+          <div className="flex sm:flex-col items-center justify-center gap-2">
+        
           {/* price */}
           <div className="flex flex-col items-end">
           <p className="text-[10px] text-slate-500">1 deal</p>
           <span className="text-xl ">Rs 63,500</span>
           <p className="text-[10px] text-slate-500">Rs 1,424 total</p>
           </div>
+         
           {/* cta */}
           <div onClick={()=>handleClickSelectFlight(data.airline,data.id)} className="px-3  py-1 gap-x-2 flex items-center justify-center bg-[#05203c] text-white rounded-md  hover:cursor-pointer  ">
           <span  className=" ">Select</span>
           <FaArrowRight />
           </div>
+
           </div>
 
           </div>
@@ -214,8 +217,35 @@ const filterArray=flightsArray
                  <ShowSelectedFlightFinal id={id} data={filterArray} checkAirline={checkAirline}  country={country} depart={depart} finalcity={finalcity} setShowSelectFlight={setShowSelectFlight}  /> 
                  </div>
         </div>
-         </div></div>
+         </div>
+
+        {/* Adds */}
+        <div className="w-[300px] hidden xl:block bg-green-400  ">
+        <div className="w-full bg-red-400 flex flex-col    space-y-4 items-center justify-center">
+         {/* 1 */}
+         <div className="w-full flex flex-col items-center justify-center space-y-3 p-3 bg-white rounded-md shadow-lg">
+             {/* Image */}
+            <div className="flex items-center justify-center space-x-1">
+            <Image height={70} width={70} src="/adds_1.svg" alt="adds_1" />    
+            <Image height={70} width={70} src="/adds_2.svg" alt="adds_1" />    
+            <Image height={70} width={70} src="/adds_3.svg" alt="adds_1" />    
+             </div>
+        </div>
+
+        </div>
+        </div>
+     
+         
+         </div>
     )
 }
 
 export default PlaneFlights
+
+
+
+
+
+
+
+// car_adds_1
