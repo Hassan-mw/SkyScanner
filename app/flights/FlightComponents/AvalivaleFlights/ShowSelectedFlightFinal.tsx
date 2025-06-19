0b09b5f40b389280c59b1c0b8f289e79ad217a3f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-06-19 13:49:35 +0500

    Updating Frontend Data

--- app/flights/FlightComponents/AvalivaleFlights/ShowSelectedFlightFinal.tsx
+++ app/flights/FlightComponents/AvalivaleFlights/ShowSelectedFlightFinal.tsx
@@ -7,126 +7,149 @@ import { Jost } from 'next/font/google'
    weight:['500']
  })
 import { FaArrowLeft } from "react-icons/fa6";
+import { IoMdAirplane } from "react-icons/io";
 
 function ShowSelectedFlightFinal({id,checkAirline,country,depart,finalcity,setShowSelectFlight,data}) {
     const filterArray=data.filter((data)=>data.id===id)
     return (
- <div className="w-full flex items-start justify-center    ">
-   <div className="w-full flex flex-col items-center justify-center bg-[#05203c] ">
+ <div className="w-full flex items-start justify-center ">
 
+  <div className="w-full flex flex-col items-center justify-center bg-[#05203c]">
+    
     {/* Navbar */}
-    <div className="max-w-screen-xl flex flex-col items-center justify-center w-full py-3   ">
-        
-        {/* top */}
-        <div className="relative w-full flex items-center justify-center p-2">
-                
-           <div onClick={()=>setShowSelectFlight(false)} className="text-white absolute top-3  left-3 hover:cursor-pointer w-44  flex items-center justify-start gap-x-2">
-              <FaArrowLeft size={12} />
-              <p className="text-sm">Back to result </p>
-            </div>
-          
-            <div className="w-full flex items-center justify-center text-white   gap-x-1 ">
-                <Link  href="/"><Image height={30} width={30}  src="/skyscanner.gif" alt="main_logo" /></Link>
-                <span className="pt-1">SkyScanner</span>
-            </div>
-
+    <div className="max-w-screen-xl flex flex-col items-center justify-center w-full py-6">
+      
+      {/* top */}
+      <div className="relative w-full flex items-center justify-center p-2">
+        <div onClick={() => setShowSelectFlight(false)} className="text-white absolute top-3 left-3 hover:cursor-pointer w-44 flex items-center justify-start gap-x-2">
+          <FaArrowLeft size={12} />
+          <p className="text-sm">Back to result</p>
         </div>
-
-
-
-        {/* bottom */}
-        <div className="flex  flex-col space-y-1 text-white  w-full  items-start p-3 ">
-            <span className={`${jost.className} text-5xl font-semibold `}> {depart}</span>
-            <span className={`${jost.className} text-sm font-semibold `}>1 traveller - Economy class</span>
+        <div className="w-full flex items-center justify-center text-white gap-x-1">
+          <Link href="/">
+            <Image height={30} width={30} src="/skyscanner.gif" alt="main_logo" />
+          </Link>
+          <span className="pt-1">SkyScanner</span>
         </div>
+      </div>
 
-
+      {/* bottom */}
+      <div className="flex flex-col space-y-1 text-white w-full items-start px-2">
+        <span className={`${jost.className} text-5xl font-semibold`}>{depart}</span>
+        <span className={`${jost.className} text-sm font-semibold`}>1 traveller - Economy class</span>
+      </div>
     </div>
 
+   {/* Show real data */}
+   <div className=" w-full  flex items-center justify-center bg-[#eff3f8] p-3">
+     <div className=" w-full max-w-[900px] flex flex-col items-center justify-center space-y-5">
 
-
-
-    {/* Show real data */}
-    <div className="flex items-center justify-center w-full bg-[#eff3f8] ">
-    <div className="flex flex-col w-full  p-3 space-y-3  max-w-screen-lg">
-       {/* Outbonding -- Return */}
-       <div className="flex items-center justify-between w-full ">
-       
-        <div className="flex items-center justify-center gap-x-1 border-b border-slate-950">
-             <span className={`${jost.className} text-md`}>Outbonding :</span>
-             <span style={{fontWeight:300}} className={`${jost.className} text-slate-600 text-md`}> 1 Feb,2025 </span>
+        {/* Outbonding -- Return */}
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center justify-center gap-x-1 border-b border-slate-950">
+            <span className={`${jost.className} text-md`}>Outbonding :</span>
+            <span style={{ fontWeight: 300 }} className={`${jost.className} text-slate-600 text-md`}>1 Feb,2025</span>
+          </div>
+          <div className="flex items-center justify-center gap-x-1 border-b border-slate-950">
+            <span className={`${jost.className} text-md`}>Return :</span>
+            <span style={{ fontWeight: 300 }} className={`${jost.className} text-slate-600 text-md`}>25 Feb,2025</span>
+          </div>
         </div>
-        <div className="flex items-center justify-center gap-x-1 border-b border-slate-950">
-             <span className={`${jost.className} text-md`}>Return :</span>
-             <span style={{fontWeight:300}} className={`${jost.className} text-slate-600 text-md`}> 25 Feb,2025 </span>
-        </div>
-       
 
-       </div>
-       {/* fligthData */}
-         <div className="w-full h-full grid grid-cols-1  gap-3 ">
-            {filterArray.map((data,index)=>
-             <div key={index} className=" w-full h-[200px] bg-white p-5  rounded-2xl flex items-center justify-center space-x-4 border ">
-              <div className="w-full flex flex-col sm:flex-row  items-center justify-center space-y-3 md:space-y-0 md:space-x-2  ">
-               <div className="w-full flex items-center justify-between  ">
-                <div><img className="h-6" src="/pia.jpeg" alt="pia-logo"/></div>
-                <div className="flex  items-center justify-center space-x-3">
-                    <div className="flex flex-col items-start justify-center space-y-5">  
-                       <div className="font-semibold">{data.startTime}</div>
-                       <div>UET</div>
+      <div className="flex flex-col sm:flex-row w-full  gap-3 ">
+     
+        {/* flightData */}
+        <div className="w-full h-full grid grid-cols-1 gap-3">
+          {filterArray.map((data, index) => (
+            <div key={index} className="w-full h-[200px] bg-white p-5 rounded-md flex items-center justify-center space-x-4 border">
+              <div className="w-full flex flex-col sm:flex-row items-center justify-center space-y-3 md:space-y-0 md:space-x-2">
+                
+                {/* Plane_Depart_Return */}
+                <div className="w-full pl-2 flex flex-col sm:col-span-2 items-center justify-center space-y-1 md:space-y-0 md:space-x-2">
+                  
+                  {/* 1 */}
+                  <div className="w-full flex items-center justify-between gap-x-12">
+                    <div>
+                      <Image height={110} width={110} src="/pia.png" alt="pia-logo" />
                     </div>
-                    <div className="flex flex-col  ">
-                       <div className="text-xs">{data.hours} hours</div>
-                       <div className="flex items-center justify-center space-x-4 ">
-                        <div className="border-t-2 pb-2 min-w-[60px] border-slate-400"></div><div className=""><ion-icon name="airplane-outline"></ion-icon></div>
-                       </div>
-                        <div>{data.stop}</div>
+                    <div className="w-full flex items-center justify-center space-x-3">
+                      {/* Plane_start */}
+                      <div className="flex flex-col items-start justify-center space-y-1">
+                        <span className="font-semibold">{data.startTime}</span>
+                        <span className="text-slate-400 text-xs">UET</span>
+                      </div>
+
+                      {/* Middle_body */}
+                      <div className="w-full flex flex-col items-center">
+                        <span className="text-xs">{data.hours} hours</span>
+                        <div className="w-full flex items-center justify-center space-x-4">
+                          <span className="border-t-2 w-full border-slate-200"></span>
+                          <span className="rotate-90"><IoMdAirplane /></span>
+                        </div>
+                        <span className="text-xs">{data.stop}</span>
+                      </div>
+
+                      {/* Plane_End */}
+                      <div className="flex flex-col items-start justify-center space-y-1">
+                        <span className="font-semibold">{data.endTime}:00</span>
+                        <span className="text-slate-400 text-xs">UET</span>
+                      </div>
                     </div>
-                    <div className="flex flex-col items-start justify-center space-y-5">
-                       <div  className="font-semibold">{data.endTime}</div>
-                       <div>UET</div>
+                  </div>
+
+                  {/* 2 */}
+                  <div className="w-full flex items-center justify-between border-b pb-2 border-slate-300 sm:border-none gap-x-12">
+                    <div>
+                      <Image height={110} width={110} src="/pia.png" alt="pia-logo" />
                     </div>
+                    <div className="w-full flex items-center justify-center space-x-3">
+                      <div className="flex flex-col items-start justify-center space-y-1">
+                        <span className="font-semibold">{data.startTime}</span>
+                        <span className="text-slate-400 text-xs">UET</span>
+                      </div>
+                      <div className="w-full flex flex-col items-center">
+                        <span className="text-xs">{data.hours} hours</span>
+                        <div className="w-full flex items-center justify-center space-x-4">
+                          <span className="border-t-2 w-full border-slate-200"></span>
+                          <span className="rotate-90"><IoMdAirplane /></span>
+                        </div>
+                        <span className="text-xs">{data.stop}</span>
+                      </div>
+                      <div className="flex flex-col items-start justify-center space-y-1">
+                        <span className="font-semibold">{data.endTime}:00</span>
+                        <span className="text-slate-400 text-xs">UET</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-               </div>
-            
               </div>
-               <div>🖤</div>
-             </div>
-            )}
-            
-           
-          
-                 </div>
-        {/* Select prising */}
-        <div className="w-full flex items-center justify-between   bg-white p-3 rounded-2xl">
-            <div className="flex flex-col items-start space-y-1">
-                    {/* Self transfer */}
-                <div className="flex items-center justify-center space-x-2">
-                    {/* <ion-icon name="alert-circle-outline"></ion-icon> */}
-                    <div className="text-sm">Self-transfer </div>
-                </div>
-                    {/* Protection transfer */}
-                <div className="flex items-center justify-center space-x-2">
-                    {/* <ion-icon name="checkmark-outline"></ion-icon> */}
-                    <div className="text-sm">Transfer protection </div>
-                </div>
-                    {/* Self transfer */}
-                <div className="flex items-center justify-center space-x-2">
-                {/* <ion-icon name="bag-check-outline"></ion-icon> */}
-                {/* <ion-icon name="bag-add-outline"></ion-icon> */}
-                {/* <ion-icon name="lock-closed-outline"></ion-icon> */}
-                </div>
+
+              <div>🖤</div>
             </div>
-                <div className="flex flex-col  items-center justify-start space-y-2 ">
-                    <div className="border px-6 py-1 rounded-xl  bg-slate-800 text-white hover:bg-slate-700 hover:cursor-pointer">Select</div>
-                    <div className="text-xl ">Rs 63,500</div>
-                </div>
-                </div>
-         </div>
-         </div>
-         </div>
-        
+          ))}
         </div>
+
+        {/* Select pricing */}
+        <div className="w-full border bg-white sm:text-center sm:w-1/3 flex sm:flex-col items-center sm:items-start justify-between sm:space-y-5 sm:py-5  p-3 rounded-md">
+          <div className=" sm:w-full flex flex-col items-start sm:items-center space-y-1">
+              <span className="text-sm">Self-transfer</span>
+              <span className="text-sm">Transfer protection</span>
+          </div>
+          <div className="sm:w-full flex flex-col  space-y-2">
+              <span className="text-xl">Rs 63,500</span>                      
+              <div className="w-20 sm:w-full flex items-center justify-center  p-1 rounded-sm bg-slate-800 text-white hover:bg-[#05203c] hover:cursor-pointer">Select</div>
+          </div>
+        </div>
+
+      </div>
+     </div>
+   </div>
+
+  </div>
+
+
+</div>
+
     )
 }
 
--- app/flights/FlightComponents/ShowFlight.tsx
+++ app/flights/FlightComponents/ShowFlight.tsx
@@ -4,7 +4,7 @@ import ShowSelectedFlightFinal from './AvalivaleFlights/ShowSelectedFlightFinal'
 const ShowFlight = ({showSelectFlight,id,filterArray,checkAirline,country,depart,finalcity,setShowSelectFlight}) => {
   return (
     <div className={`${  showSelectFlight  && 'w-full h-full flex items-center justify-center  ' }`}>
-        <div className={`w-full   flex flex-col transform transition-transform  fixed bg-white z-50 ease-in-out duration-500 ${showSelectFlight ? 'translate-x-0' :'translate-x-full' } shadow-2xl    h-[100vh] w-[100vw] top-0 left-0` }>
+        <div className={`w-full   flex flex-col transform transition-transform  fixed bg-[#eff3f8] z-50 ease-in-out duration-500 ${showSelectFlight ? 'translate-x-0' :'translate-x-full' } shadow-2xl    h-[100vh] w-[100vw] top-0 left-0` }>
           <ShowSelectedFlightFinal id={id} data={filterArray} checkAirline={checkAirline}  country={country} depart={depart} finalcity={finalcity} setShowSelectFlight={setShowSelectFlight}  /> 
         </div>
    </div>
