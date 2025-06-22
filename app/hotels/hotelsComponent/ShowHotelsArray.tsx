6a5baf84385d39f30899727631b72d942f13a809
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-06-22 22:53:25 +0500

    Updating Frontend Data

--- app/hotels/hotelsComponent/ShowHotelsArray.tsx
+++ app/hotels/hotelsComponent/ShowHotelsArray.tsx
@@ -16,40 +16,40 @@ function ShowHotelsArray({country,text}) {
         <div className="w-full h-full flex flex-col   sm:flex-row  ">
 
         {/* IMAGE */}
-         <div className="w-full bg-blue-500 max-h-60 sm:w-[40%]  overflow-hidden rounded-t-md sm:rounded-none sm:rounded-l-2xl">
+         <div className="w-full bg-blue-500 max-h-60 sm:w-[40%]  overflow-hidden rounded-t-md sm:rounded-none sm:rounded-l-sm">
           <img className="object-cover w-full  sm:h-full  " src="/hotel.webp"/>
           </div>
         {/* CONTENT */}
-         <div className="flex max-h-60 flex-col py-2 sm:justify-center md:flex-row bg-white overflow-hidden w-full  border rounded-b-2xl sm:rounded-none sm:rounded-r-2xl  ">
+         <div className="flex max-h-60 flex-col py-2 sm:justify-center md:flex-row bg-white overflow-hidden w-full  border rounded-b-2xl sm:rounded-none sm:rounded-r-sm  ">
           {/* Details*/}
          <div className="flex flex-col w-full items-start justify-center  md:justify-start md:pt-6 p-2 md:px-7 space-y-2 md:border-r  border-slate-300  ">
           <div className="flex  items-center  md:space-x-9 justify-between w-full space-x-3">
             <span className={`${jost.className} text-sm  md:text-lg lg:text-2xl font-semibold`}>JOUDYAN Red Sea Mall Jeddah By Elaf</span><span className="text-sm">⭐⭐⭐⭐⭐</span></div>
-          <span style={{fontWeight:300}} className={`${jost.className}  text-sm text-slate-500`}>12.10 km from city centre</span>
+          <span style={{fontWeight:300}} className={`${jost.className}  text-sm text-gray-700`}>12.10 km from city centre</span>
             <div  className="flex items-center justify-start gap-x-1 ">
                      <div  className="flex items-end justify-end  ">
                         <span className={`${jost.className} font-semibold text-sm text-black `}>4.2</span>
-                        <span  style={{fontWeight:200}} className={`${jost.className} text-[11px] text-slate-500`}>/5</span> 
+                        <span  className={`text-thin text-[11px] text-gray-700`}>/5</span> 
                      </div>
                         <span className={`${jost.className} font-semibold text-sm text-black `}>Very good</span>
-                        <span style={{fontWeight:200}} className={`${jost.className} pt-1  text-[12px] text-slate-400`}>124 review</span>
+                        <span className={`font-thin pt-1  text-[12px] text-slate-600`}>124 review</span>
                      </div>
          {/* <div className="flex items-start justify-start space-x-3 "><span className="font-semibold">4.5</span> <span className="text-2xl "><ion-icon name="infinite-outline"></ion-icon></span></div>  */}
           </div>
           {/* Pricing */}
-          <div className="flex md:flex-col md:justify-center md:space-y-7 md:w-[40%] items-center justify-between w-full  p-2 space-y-2 ">
+          <div className="flex  md:flex-col md:justify-center md:space-y-7 md:w-[40%] items-center justify-between w-full  p-2 space-y-2 ">
        <div className="flex flex-col items-center justify-end md:w-full md:items-end ">
       <div className="text-xs font-semibold hidden md:block"> Lowest price </div>
       <div className="text-xs hidden md:block">we found for this hotel </div>
       <div className=" bg-white hidden md:block"><img className="w-16 " src="/d_ct.png"/></div>
         
-        <div className=" flex flex-col items-start justify-start">
-        <span className="text-xs text-slate-500">From</span>
-        <span className="text-xl font-semibold ">Rs 15,517</span>
-        <span className="text-xs text-slate-500">a night</span>
+        <div className=" flex flex-col items-start justify-start md:items-end md:justify-end">
+        <span className="text-xs text-gray-700">From</span>
+        <span className="text-md font-semibold ">Rs 15,517</span>
+        <span className="text-xs text-gray-700">a night</span>
         </div>
           </div>
-          <Link  href={`/hotels/${country}/${hotelname}`} className="border bg-[#05203c] hover:bg-blue-900 rounded-lg font-semibold hover:cursor-pointer text-white px-3 lg:px-5 py-2 lg:w-full flex items-center justify-center">{text}</Link>
+          <Link  href={`/hotels/${country}/${hotelname}`} className={`${jost.className} lg:w-full flex items-center justify-center border bg-[#05203c] hover:bg-blue-900 rounded-sm font-semibold hover:cursor-pointer text-white px-3 lg:px-5 py-2`}>{text}</Link>
           </div>
          
          </div>
--- app/hotels/hotelsComponent/ShowMapOption.tsx
+++ app/hotels/hotelsComponent/ShowMapOption.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import { GoogleMap, LoadScript, Marker, MarkerF } from '@react-google-maps/api';
 import {MapContainer,TileLayer}  from "react-leaflet"
 import "leaflet/dist/leaflet.css"
+import Image from 'next/image';
+import { CiMap } from 'react-icons/ci';
+import { FaMap } from 'react-icons/fa6';
 
 const containerStyle = {
   width: '100%',
@@ -57,56 +60,105 @@ const points = [
 
 function ShowMapOption({country}:{country:string}) {
     return (
-      <div className='w-full h-[360px]  space-y-3'>
+      <div className='w-full  h-[400px]  space-y-3'>
         
    <h1 className="w-full flex items-start justify-start  text-2xl  sm:text-4xl font-semibold ">See all hotel in {country}</h1>   
-   <MapContainer className='h-[300px] ' center={[48.8566,2.3522]} zoom={13}>
-      <TileLayer 
-        
-         attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
+    <div className='w-full relative  h-[300px]  space-y-3'>
+    <Image height={1200} width={1200} className='w-full bg-green-500 rounded-md h-[300px] object-cover' src="/google_map_image.webp" alt='map_image'/>
+    <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center '>
 
+    <div className=' flex items-center justify-center gap-x-2 bg-[#05203c] p-3 rounded-md w-42 text-white '><FaMap/><span>Explore map</span></div>
+    </div>
+    </div>
 
    
-   </MapContainer>
- 
-  <div id="map" className='h-[500px] w-[500px]'></div>
-
-
-
-
-   {/* <LoadScript googleMapsApiKey="AIzaSyAGzV9Dsxun971k4mu6QMfG3yF1Fbar9gE">
-      <GoogleMap 
-        mapContainerStyle={containerStyle}
-        center={centerDefault}
-        zoom={11}
-      >
-        
-      {points.map((data,index)=>  <MarkerF key={index} position={data} ></MarkerF> )}
-      </GoogleMap>
-    </LoadScript> */}
-       </div>
-        // <div className="w-full h-full ">
-      //   <div className="w-full h-full   overflow-hidden  rounded-2xl flex flex-col justify-between"> {/* Increased height */}
-       
-      // {/* <div className=" w-full overflow-hidden  min-h-[100vh] "><Map/></div> */}
-
-      //   <div>
+   
      
-      //   </div>
-      //   {/* <div className=" absolute  top-1/2  left-1/2">
-      // <div className="bg-slate-800 p-2 max-w-[300px] rounded-sm hover:bg-slate-700 flex items-center hover:cursor-pointer justify-center space-x-3 " > 
-      //     <div className=" text-white text-base">Explore map</div> 
-      //      <div className="text-xl -rotate-45 text-white "><ion-icon name="log-out-outline"></ion-icon></div>
-            
-      //       </div>
-      // </div> */}
-      
-      //       </div>
-            // </div>
+            </div>
             
     )
 }
 
 export default ShowMapOption
+
+
+// 'use client'
+// // components/Map.js
+// import React from 'react';
+// import { GoogleMap, LoadScript, Marker, MarkerF } from '@react-google-maps/api';
+// import {MapContainer,TileLayer}  from "react-leaflet"
+// import "leaflet/dist/leaflet.css"
+
+// const containerStyle = {
+//   width: '100%',
+//   height: '100%'
+// };
+
+// const centerDefault = {
+  
+//   lat: 21.5400,  
+//   lng: 39.1495
+// };
+// const points = [
+//   { 
+//     lat: 21.4225,  
+//     lng: 39.8262,  // Jeddah by Elef
+//   },
+//   { 
+//     lat: 21.5423,  
+//     lng: 39.1732,  // Velvet Hotel, Jeddah
+//   },
+//   { 
+//     lat: 21.5760,  
+//     lng: 39.1565,  // WA Hotel, Jeddah
+//   },
+//   { 
+//     lat: 21.5921,  
+//     lng: 39.1761,  // Movenpick Hotel Jeddah
+//   },
+//   { 
+//     lat: 21.5757,  
+//     lng: 39.1793,  // InterContinental Jeddah
+//   },
+//   { 
+//     lat: 21.5104,  
+//     lng: 39.1845,  // Radisson Blu Hotel, Jeddah
+//   },
+//   { 
+//     lat: 21.5916,  
+//     lng: 39.1570,  // The Ritz-Carlton, Jeddah
+//   },
+//   { 
+//     lat: 21.5400,  
+//     lng: 39.1495,  // Hilton Jeddah
+//   },
+//   { 
+//     lat: 21.5436,  
+//     lng: 39.1725,  // Jeddah Marriott Hotel
+//   }
+// ];
+
+
+// // function ShowMapOption({country}:{country:string}) {
+// //     return (
+// //       <div className='w-full h-[400px]  space-y-3'>
+        
+// //    <h1 className="w-full flex items-start justify-start  text-2xl  sm:text-4xl font-semibold ">See all hotel in {country}</h1>   
+// //    <MapContainer className='h-[1200px] ' center={[48.8566,2.3522]} zoom={13}>
+// //       <TileLayer 
+        
+// //          attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// //           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+// //       />
+
+
+   
+// //    </MapContainer>
+ 
+// //        </div>
+
+            
+// //     )
+// // }
+
+// // export default ShowMapOption
