3cdf4bc93d523c5f4ec0b59a6973ec64307680dd
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-06-29 17:30:35 +0500

    Updating Frontend Data

---
+++ app/Backend/server.js
@@ -0,0 +1,30 @@
+const express=require('express')
+const app=express()
+const Pool = require("./Pool/pool");
+
+
+
+
+
+
+  
+
+
+  
+Pool.connect({
+    host:"localhost",
+    port:5432,
+    database:'SkyScanner',
+    user:'postgres',
+    password:'hassan'
+}).then(()=>{
+    console.log('Connected to Database 🥳🥳🥳🥳🥳')
+}).catch((err)=>{
+    console.log(err,"Can't connected to database 💥💥💥")
+})
+
+
+app.listen('5000',()=>{
+   console.log(`server was runing on port 5000`)
+
+})
