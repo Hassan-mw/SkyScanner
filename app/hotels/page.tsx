503e5c22b6fbbaf5378271be9354cc69a2fa0e04
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-07-14 23:58:34 +0500

    Updating Frontend Data

--- app/hotels/page.tsx
+++ app/hotels/page.tsx
@@ -1,35 +1,34 @@
+import { fetchAllHotelsData } from "../API/hotels";
+import CustomHotelsLogo from "./hotelsComponent/CustomHotelsLogo";
+import CustomSearchBarHotel from "./hotelsComponent/CustomSearchBarhotel";
+import FastFactMainHotel from "./hotelsComponent/FastFactMainHotel";
+import HotelNearOwncity from "./hotelsComponent/HotelNearOwnCity";
+import HotelPlace from "./hotelsComponent/HotelPlace";
+import ShowCurrentPageHotel from "./hotelsComponent/ShowCurrentPageHotel";
 
-
-import { fetchAllHotelsData } from "../API/hotels"
-import CustomHotelsLogo from "./hotelsComponent/CustomHotelsLogo"
-import CustomSearchBarHotel from "./hotelsComponent/CustomSearchBarhotel"
-import FastFactMainHotel from "./hotelsComponent/FastFactMainHotel"
-import HotelNearOwncity from "./hotelsComponent/HotelNearOwnCity"
-import HotelPlace from "./hotelsComponent/HotelPlace"
-import ShowCurrentPageHotel from "./hotelsComponent/ShowCurrentPageHotel"
-
-
-
-const page = async() => {
-  // const data=await fetchAllHotelsData()
-  // console.log(data)
+const Page = async () => {
   return (
-    <div className="w-full h-full flex flex-col space-y-10 items-center  bg-white overflow-y-auto  overflow-x-hidden">
-         <div className="bg-[#05203c] flex items-center justify-center w-full"><CustomSearchBarHotel/></div>
-      <div className="flex flex-col items-start justify-start space-y-16 w-full p-2   max-w-screen-xl">
-      <ShowCurrentPageHotel/>
- 
-      {/* //! Hotels Logo */}
-      {/* <CustomHotelsLogo  /> */}
+    <div className="w-full h-full flex flex-col space-y-10 items-center bg-white overflow-y-auto overflow-x-hidden">
+      
+      {/* Search Bar */}
+      <div className="bg-[#05203c] flex items-center justify-center w-full">
+        <CustomSearchBarHotel />
       </div>
-      <div className="p-3 flex flex-col space-y-20 items-center w-full h-full bg-white max-w-screen-xl ">
-        
-      <HotelNearOwncity/>
-      <HotelPlace/>
-      <FastFactMainHotel/>
-      </div>
-      </div>
-  )
-}
 
-export default page
+      {/* Page Heading Section */}
+      <div className="flex flex-col items-start justify-start space-y-16 w-full p-2 max-w-screen-xl">
+        <ShowCurrentPageHotel />
+        <CustomHotelsLogo />
+      </div>
+
+      {/* Hotels Content Section */}
+      <div className="p-3 flex flex-col space-y-20 items-center w-full h-full bg-white max-w-screen-xl">
+        <HotelNearOwncity />
+        <HotelPlace />
+        <FastFactMainHotel />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
