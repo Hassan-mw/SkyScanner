1733b2ff56d012d201df69136fce5753a234c291
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-07-15 01:24:24 +0500

    Updating Frontend Data

--- app/hotels/hotelsComponent/Map.tsx
+++ app/hotels/hotelsComponent/Map.tsx
@@ -1,105 +1,53 @@
-'use client'
-// components/Map.js
-import React from 'react';
-// import { GoogleMap, LoadScript, Marker, MarkerF } from '@react-google-maps/api';
-import {MapContainer,Popup,TileLayer,Marker}  from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import { FaMapMarkerAlt } from 'react-icons/fa';
+'use client';
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
 
+// Static hotel points
+const points = [
+  { lat: 21.4225, lng: 39.8262 },
+  { lat: 21.5423, lng: 39.1732 },
+  { lat: 21.5760, lng: 39.1565 },
+  { lat: 21.5921, lng: 39.1761 },
+  { lat: 21.5757, lng: 39.1793 },
+  { lat: 21.5104, lng: 39.1845 },
+  { lat: 21.5916, lng: 39.1570 },
+  { lat: 21.5400, lng: 39.1495 },
+  { lat: 21.5436, lng: 39.1725 }
+];
 
+// Custom Leaflet marker icon
+const customIcon = new L.Icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
+const Map = ({data}) => {
+  const center = { lat: 31.5639, lng: 74.3203}; // You can adjust this center
+ 
+      const x = data?.map(item => ({
+    lat: parseFloat(item.latitude),
+    lng: parseFloat(item.longitude),
+  }));
+  console.log(x)
+  return (
+    <div className="w-full h-[500px]">
+      <MapContainer center={[center.lat, center.lng]} zoom={13} className="h-full w-full">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-
-const centerDefault = {
-  
-  lat: 21.5400,  
-  lng: 39.1495
+        {x?.map((data, index) => (
+          <Marker key={index} position={[data.lat, data.lng]} icon={customIcon}>
+            <Popup>📍 Hotel {index + 1}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 };
 
-
-// const points = [
-//   { 
-//     lat: 21.4225,  
-//     lng: 39.8262,  // Jeddah by Elef
-//   },
-//   { 
-//     lat: 21.5423,  
-//     lng: 39.1732,  // Velvet Hotel, Jeddah
-//   },
-//   { 
-//     lat: 21.5760,  
-//     lng: 39.1565,  // WA Hotel, Jeddah
-//   },
-//   { 
-//     lat: 21.5921,  
-//     lng: 39.1761,  // Movenpick Hotel Jeddah
-//   },
-//   { 
-//     lat: 21.5757,  
-//     lng: 39.1793,  // InterContinental Jeddah
-//   },
-//   { 
-//     lat: 21.5104,  
-//     lng: 39.1845,  // Radisson Blu Hotel, Jeddah
-//   },
-//   { 
-//     lat: 21.5916,  
-//     lng: 39.1570,  // The Ritz-Carlton, Jeddah
-//   },
-//   { 
-//     lat: 21.5400,  
-//     lng: 39.1495,  // Hilton Jeddah
-//   },
-//   { 
-//     lat: 21.5436,  
-//     lng: 39.1725,  // Jeddah Marriott Hotel
-//   }
-// ];
-
-const customIcon=new L.Icon ({
-iconUrl:markerIcon.src
-
-})
-
-const Map = ({country,data}:{country:string}) => {
-    if(data?.length===0) return;
-
-  const points = data?.map(item => ({
-  lat: Number(item.latitude),
-  lng: Number(item.longitude)
-}));
-console.log(points)
-  return (
-      <div className='w-full h-full space-y-3 p-1  border-white rounded-md xl:border-green-500 xl:rounded-none xl:p-none'>
-        
-    
-    <MapContainer className='h-full z-10 ' center={[ 21.5760,   39.1565]} zoom={14}>
-       <TileLayer 
-        
-          attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-       />
-      {points?.map((data,index)=> 
-        //  <MarkerF position={data} ></MarkerF>
-       <Marker  key={index} position={data} icon={customIcon} >
-         <Popup>
-          <b>GGGGGGGGGGGGGG</b>
-         </Popup>
-       </Marker>
-         )}
-         
-
-
-
-   
-    </MapContainer>
- 
-        </div>
-
-            
-  )
-}
-
-export default Map
+export default Map;
