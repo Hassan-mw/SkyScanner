fd8b0c3d526d37499a8ade53347b76b95e423ec6
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-08-07 23:38:30 +0500

    Updating Data

--- app/Components_Custom/CustomDatePicker.tsx
+++ app/Components_Custom/CustomDatePicker.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import {  DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
-
 const CustomDatePicker = ({date,setDateSelect}) => {
   
   const [selectedDate, setSelectedDate] = useState(date);
