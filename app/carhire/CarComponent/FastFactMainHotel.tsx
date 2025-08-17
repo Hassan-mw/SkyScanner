3e63947c92ed07579a813313998f1c6ccf84018f
AMW <hmwsoftwaredevelopers@gmail.com>
2025-08-17 23:55:11 +0500

    Updating Data

--- app/carhire/CarComponent/FastFactMainHotel.tsx
+++ app/carhire/CarComponent/FastFactMainHotel.tsx
@@ -13,7 +13,6 @@ import Image from "next/image"
 import { IoBedSharp } from "react-icons/io5"
 import { RiPlaneLine } from "react-icons/ri";
 import { FaCalendarAlt } from "react-icons/fa"
-
 const jost=Jost({
     subsets:['latin'],
     weight:['500']
