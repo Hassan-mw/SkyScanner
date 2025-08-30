6b0d217fc94fe31b520f3e163dbc7ad9e6f71e17
AMW <hmwsoftwaredevelopers@gmail.com>
2025-08-30 23:45:19 +0500

    Updating Data

--- app/carhire/CarComponent/ShowCurrentPageHotels.tsx
+++ app/carhire/CarComponent/ShowCurrentPageHotels.tsx
@@ -32,7 +32,6 @@ function ShowCurrentPageHotel() {
         <span className={`${jost.className} text-sm `}>Search for cheap car rental in seconds – anywhere in the world</span>
         </div>
 
-
         {/* 2 */}
         <div className="flex items-start sm:flex-col lg:flex-row text-black   space-x-3 sm:space-x-0 sm:space-y-2 lg:space-y-0  lg:space-x-3">
         <div className="text-sm  pt-1"><FaCalendarAlt size={20} /></div>
