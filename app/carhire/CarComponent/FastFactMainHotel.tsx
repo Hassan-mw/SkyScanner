1f9fbf6135aae6018895636f671d0c13fbe6b027
AMW <hmwsoftwaredevelopers@gmail.com>
2025-09-22 23:57:08 +0500

    Updating Data

--- app/carhire/CarComponent/FastFactMainHotel.tsx
+++ app/carhire/CarComponent/FastFactMainHotel.tsx
@@ -267,6 +267,7 @@ function FastFactMainHotel() {
               <IoIosArrowForward />   
             </Link>
 
+
             <Link href="/flights" className="w-full flex hover:pointer items-center justify-between border-b border-black pb-2">
               <div className="flex items-center justify-center gap-x-3">
                  <div className="rotate-45"> <RiPlaneLine  size={20} /> </div>
